3ce24e08c124e45923ee565268b8015fca3799c2
Roman Paravaev <105551615+falkomerr@users.noreply.github.com>
2025-02-12 19:50:39 +0300

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,21 @@ import { Navigation } from './Navigation.tsx';
 
 export const Header = () => {
   return (
-    <div className="absolute top-0 z-50 flex w-full justify-between px-20 py-[2rem]">
+    <div className="absolute top-0 z-50 flex w-full items-center justify-between px-20 py-[2rem]">
+      <div></div>
+      <div></div>
       <div></div>
       <Navigation />
+      <div className="flex items-center gap-x-3">
+        <LeftArrow />
+        <a
+          href="https://x.com/xhunterai_?s=21"
+          target="_blank"
+          className="z-50 cursor-pointer transition-all duration-200 hover:scale-[1.1]">
+          <img src="/twitter.svg" alt="twitter" width={35} height={35} />
+        </a>
+        <RightArrow />
+      </div>
       <div className="flex cursor-pointer items-center">
         <img src="/logo.svg" alt="logo" width={60} height={60} />
         <p className="font-symtext text-[1.625rem] text-white">XHUNTER</p>
@@ -12,3 +24,30 @@ export const Header = () => {
     </div>
   );
 };
+
+const LeftArrow = () => (
+  <svg
+    width="8"
+    height="14"
+    viewBox="0 0 8 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M2.60156 6.16135V0.958221H7.80469V3.55978H5.20313V11.3645H7.80469V13.966H2.60156V8.76291H0V6.16135H2.60156Z"
+      fill="white"
+    />
+  </svg>
+);
+const RightArrow = () => (
+  <svg
+    width="8"
+    height="14"
+    viewBox="0 0 8 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M5.39844 6.16135V0.958221H0.195312V3.55978H2.79687V11.3645H0.195312V13.966H5.39844V8.76291H8V6.16135H5.39844Z"
+      fill="white"
+    />
+  </svg>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,23 @@
 import { useRef } from 'react';
 import TextCursorProximity from '../components/text-cursor-proximity.tsx';
 import { MagneticButton } from '../components/magnetic-button.tsx';
-import { CardSpotlight } from '../components/card-spotlight.tsx';
 import { Console } from '../components/Console.tsx';
+import {
+  CardBody,
+  CardContainer,
+  CardItem,
+} from '../components/ui/3d-card.tsx';
 
 const text = `$ agent_status
 name: XHUNTER
 eliza_runtime: v0.1.9
 source: https://xhunterai.com/
 Loading...`;
+
+const cardTexts = [
+  'It analyzes patterns of aggressive trading, detects moments of excess confidence or panic, and signals when volatility is driven by human behavior rather than fundamentals.',
+  'By observing those who push the market to extremes, XHUNTER provides a deeper layer of insight—one based not on price action alone, but on the psychology of those moving it',
+];
 
 export default function Index() {
   return (
@@ -27,34 +36,15 @@ const Text = () => {
 
   return (
     <div
-      className="absolute bottom-1/2 left-[52%] right-[60%] top-1/2 flex h-fit w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center p-6 sm:p-12 md:p-16 lg:p-24"
+      className="absolute bottom-1/2 left-[52%] right-[60%] top-[55%] flex h-fit w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center p-6 sm:p-12 md:p-16 lg:p-24"
       ref={containerRef}>
       <div
-        className="relative flex w-fit cursor-pointer flex-col items-start justify-center px-6 py-20 text-white"
+        className="relative flex w-fit cursor-pointer flex-col items-start justify-center px-6 text-white"
         style={{
           backgroundColor: 'transparent',
           minHeight: '250px',
         }}>
         <div className="ml-4 flex flex-col justify-center pl-6 pt-4 leading-none">
-          <div className="flex items-center gap-x-4">
-            <p className="max-w-[32.8125rem] font-homespun text-[1.5rem] leading-[2rem]">
-              Subscribe to our
-            </p>
-
-            <a
-              href="https://x.com/xhunterai_?s=21"
-              target="_blank"
-              className="z-50 cursor-pointer transition-all duration-200 hover:scale-[1.1]">
-              <img src="/twitter.svg" alt="twitter" width={48} height={48} />
-            </a>
-            <p className="max-w-[32.8125rem] font-homespun text-[1.5rem] leading-[2rem]">
-              account.
-            </p>
-          </div>
-          <p className="-mt-6 max-w-[32.8125rem] font-homespun text-[1.5rem]">
-            <br /> Keep up to date with the latest news!
-          </p>
-
           <TextCursorProximity
             label="x.hunterai"
             className="font-symtext text-[7rem] uppercase will-change-transform"
@@ -79,18 +69,24 @@ const Text = () => {
         </div>
 
         <MagneticButton>
-          <button className="mb-[6.25rem] mt-[3.375rem] rounded-xl bg-[#0D99FF] px-10 py-4 font-homespun text-xl text-white transition-colors duration-300 hover:opacity-90">
+          <button className="mb-[5.25rem] mt-[3.375rem] rounded-xl bg-[#0D99FF] px-10 py-4 font-homespun text-xl text-white transition-colors duration-300 hover:opacity-90">
             Go enter
           </button>
         </MagneticButton>
 
-        <CardSpotlight className="ml-8 h-fit w-[36.8125rem]">
-          <p className="relative z-[999] font-homespun text-[1.75rem]">
-            It analyzes patterns of aggressive trading, detects moments of
-            excess confidence or panic, and signals when volatility is driven by
-            human behavior rather than fundamentals.
-          </p>
-        </CardSpotlight>
+        <div className="ml-8 flex gap-x-[2.625rem]">
+          {cardTexts.map((item, i) => (
+            <CardContainer className="inter-var" key={i}>
+              <CardBody className="group/card relative h-[16.5rem] w-[32.75rem] rounded-xl border border-[#686E72] bg-gradient-to-b from-[#051420] to-[#03111C] px-[3.375rem] py-[1.875rem] dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]">
+                <CardItem
+                  translateZ="50"
+                  className="relative z-[999] font-homespun text-[1.4375rem]">
+                  {item}
+                </CardItem>
+              </CardBody>
+            </CardContainer>
+          ))}
+        </div>
       </div>
     </div>
   );
