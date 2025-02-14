3a6e53c64c64c731bffe13207f1c681f5a846195
Roman Paravaev <105551615+falkomerr@users.noreply.github.com>
2025-02-14 10:07:27 +0300

    fix

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,17 +2,21 @@ import { Card } from '../components/Card.tsx';
 import { Header } from '../components/Header.tsx';
 import { Footer } from '../components/Footer.tsx';
 import { CanvasRevealEffect } from '../components/canvas-reveal-effect.tsx';
+import { useLocation } from 'react-router-dom';
 
 export default ({ children }: { children: React.ReactNode }) => {
+  const { pathname } = useLocation();
   return (
     <div className="mx-auto flex w-full max-w-[100vw] flex-col items-center justify-center gap-4 overflow-hidden bg-black">
       <Card>
-        <CanvasRevealEffect
-          showGradient
-          animationSpeed={7}
-          containerClassName="bg-sky-600"
-          colors={[[125, 211, 252]]}
-        />
+        {pathname !== '/' && (
+          <CanvasRevealEffect
+            showGradient
+            animationSpeed={7}
+            containerClassName="bg-sky-600"
+            colors={[[125, 211, 252]]}
+          />
+        )}
 
         <Header />
         {children}
